afe57bb3cf0ca4a8b59ae84c9dafe408f1753616
jmperezs1 <jm.perezs1@uniandes.edu.co>
2022-04-02 17:45:37 -0500

    Aplicacion

--- Implementacion/src/modelo/Aplicacion.java
+++ Implementacion/src/modelo/Aplicacion.java
@@ -1,7 +1,17 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Aplicacion {
 
+	private ArrayList<Participante> participantes;
+	
+	private ArrayList<Proyecto> proyectos;
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
