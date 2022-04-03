042909762070773fd748208f8b7298ddab9d3325
SergioAvendano <78516071+SergioAvendano@users.noreply.github.com>
2022-04-02 21:49:55 -0500

    Update Aplicacion.java

--- Implementacion/src/modelo/Aplicacion.java
+++ Implementacion/src/modelo/Aplicacion.java
@@ -4,12 +4,25 @@ import java.util.ArrayList;
 
 public class Aplicacion {
 
-	private ArrayList<Participante> participantes;
 	
-	private ArrayList<Proyecto> proyectos;
+	private List<Participante> participantes;
 	
+	private List<Proyecto> proyectos;
 	
+	public void agregarProyecto(Proyecto nuevoProyecto)
+	{
+		proyectos.add(nuevoProyecto);
+	}
 	
+	public Participante buscarParticipante() {
+		
+	}
+	
+	public Proyecto buscarProyecto() {
+		
+	}
+	
+	public generar reporte ????
 	
 	
 	public static void main(String[] args) {
