9e176961d9750541b4779d13ed760429bf61ed73
SergioAvendano <78516071+SergioAvendano@users.noreply.github.com>
2022-04-02 21:51:06 -0500

    Update Participante.java

--- Implementacion/src/modelo/Participante.java
+++ Implementacion/src/modelo/Participante.java
@@ -6,4 +6,23 @@ public class Participante {
 	
 	private String correo;
 	
+	
+	public Participante(String elNombre, String elCorreo)
+	{
+		this.nombre = elNombre;
+		this.correo = elCorreo;
+	}
+	
+	
+	
+	public String darNombre()
+	{
+		return nombre;
+	}
+	
+	public String darCorreo()
+	{
+		return correo;
+	}
+	
 }
